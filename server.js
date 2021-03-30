ac408b2d43cf9fbbd2fdc3c8f04ad2bbf51396fc
Andrei B <ruby.project15@gmail.com>
2021-03-30 13:30:39 +0300

    modified stuff

--- server.js
+++ server.js
@@ -46,10 +46,11 @@ app.post('/send', (req, res) =>{
 });
 
 //serve static assets in production
-if(process.env.NODE ==='production'){
-    //set static folder
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
