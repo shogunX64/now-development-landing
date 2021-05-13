3e35d84878d58761030527a4462e5e3fd7f6405e
Andrei B <ruby.project15@gmail.com>
2021-05-13 11:34:15 +0300

    added client side form validations

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -33,13 +33,13 @@ const Form = () => {
                         <div className="row">
                             <form onSubmit={onSubmit} className="col-lg-6 text-center col-md-8 ml-auto mr-auto">
                                 <div className="input-group input-lg">
-                                    <input type="text" name="name" value={name} onChange={(e)=> setName(e.target.value)} className="form-control" placeholder="Name..."/>
+                                    <input type="text" minLength='3' name="name" value={name} onChange={(e)=> setName(e.target.value)} className="form-control" placeholder="Name..."/>
                                 </div>
                                 <div className="input-group input-lg">
-                                    <input type="text" name="mail" value={mail} onChange={(e)=> setMail(e.target.value)} className="form-control" placeholder="Mail..."/>
+                                    <input type="email" minLength='6' name="mail" value={mail} onChange={(e)=> setMail(e.target.value)} className="form-control" placeholder="Mail..."/>
                                 </div>
                                 <div className="textarea-container">
-                                    <textarea name="message" value={message} onChange={(e)=> setMessage(e.target.value)} className="form-control" rows="4" cols="80" placeholder="Type a message..."></textarea>
+                                    <textarea name="message" minLength='10' value={message} onChange={(e)=> setMessage(e.target.value)} className="form-control" rows="4" cols="80" placeholder="Type a message..."></textarea>
                                 </div>
                                 <div className="send-button">
                                     <button className="btn btn-primary btn-round btn-block btn-lg">Send Message</button>
